b144ebd6f079ced0c1388fca8dae94f621151dea
GNEHS <jayuiop@gmail.com>
2018-02-17 23:38:16 +0800

    Chapter switch animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    headerImg()
+    menuClick()
     var chapter = $('#content h1,#content h2,#content h3,#content h4,#content h5,#content h6')
     $.ripple(".items a.item,.button,a.card,.menu a.item", {
         debug: false, // Turn Ripple.js logging on/off
@@ -18,57 +20,73 @@ $(document).ready(function() {
         easing: 'linear' // The CSS3 easing function of the ripple
     });
     chapter.attr("class", "ts header")
-    headerImg()
+    InstantClick.init();
     if (chapter.length == 0) {
         $('#menu').attr('style', 'display:none')
     }
 });
+
+function menuClick() {
+    $('#menu a').click(function() {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        // 感謝網友 sam 修正 Opera 問題
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: $(this).attr('data-scroll') - 60
+        }, 200);
+    });
+
+}
 $(window).scroll(function() {
-    if ($(this).scrollTop() - 80 > $('.ts.container').offset().top) {
-        $('#menu').addClass('show')
-    } else {
-        $('#menu').removeClass('show')
-        return
-    }
+    scrollspy()
+});
+
+function scrollspy() {
 
     var chapter = $('#content h1,#content h2,#content h3,#content h4,#content h5,#content h6')
+    if ($(this).scrollTop() - 80 > $('.ts.container').offset().top && chapter.length > 0) {
+        $('#menu').addClass('show')
+    } else {
+        $('#menu').removeClass('show');
+        return;
+    }
 
     // Get container scroll position
     var fromTop = $(this).scrollTop();
 
     for (var i = 0; i < chapter.length; i++) {
-        if ($(chapter[i]).offset().top < fromTop + 20) {
+        if ($(chapter[i]).offset().top < fromTop + 61) {
             var previousChapter = $(chapter[i - 1])
             var nowChapter = $(chapter[i])
             var nextChapter = $(chapter[i + 1])
         }
     }
-    if ($('#menu .nowChapter').attr('href') != "#" + nowChapter.attr('id')) {
-        /*if ($('#menu .nowChapter').attr('href') == "#" + previousChapter.attr('id')) {
+    if (nowChapter != undefined && $('#menu .nowChapter').attr('data-scroll') != nowChapter.offset().top) {
+        if (previousChapter.length != 0 && $('#menu .nowChapter').attr('data-scroll') == previousChapter.offset().top) {
             $('#menu').addClass('toNext')
         }
-        if ($('#menu .nowChapter').attr('href') == "#" + nextChapter.attr('id')) {
+        if (nextChapter.length != 0 && $('#menu .nowChapter').attr('data-scroll') == nextChapter.offset().top) {
             $('#menu').addClass('toPrevious')
-        }*/
+        }
         $('#menu').addClass('changeing')
         setTimeout(function() { $('#menu').removeClass('changeing toNext toPrevious') }, 400);
     }
 
     if (previousChapter == undefined || previousChapter.length == 0)
-        $('#menu .previousChapter').text('-').attr('href', '#!')
+        $('#menu .previousChapter').text('-').attr('data-scroll', '')
     else
-        $('#menu .previousChapter').text(previousChapter.text()).attr('href', '#' + previousChapter.attr('id'));
+        $('#menu .previousChapter').text(previousChapter.text()).attr('data-scroll', previousChapter.offset().top);
 
     if (nowChapter == undefined || nowChapter.length == 0)
-        $('#menu .nowChapter').text('-').attr('href', '#!')
+        $('#menu .nowChapter').text('-').attr('data-scroll', '#!')
     else
-        $('#menu .nowChapter').text(nowChapter.text()).attr('href', '#' + nowChapter.attr('id'))
+        $('#menu .nowChapter').text(nowChapter.text()).attr('data-scroll', nowChapter.offset().top)
 
     if (nextChapter == undefined || nextChapter.length == 0)
-        $('#menu .nextChapter').text('-').attr('href', '#!')
+        $('#menu .nextChapter').text('-').attr('data-scroll', '')
     else
-        $('#menu .nextChapter').text(nextChapter.text()).attr('href', '#' + nextChapter.attr('id'))
-});
+        $('#menu .nextChapter').text(nextChapter.text()).attr('data-scroll', nextChapter.offset().top)
+}
 
 function headerImg() {
     if (window.sessionStorage["headerImg"]) {
