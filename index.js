4459be717ec5921215248e952e613526c88bf81c
gnehs <you@example.com>
2018-10-23 20:34:22 +0800

    優化後端搜尋

--- index.js
+++ index.js
@@ -161,10 +161,10 @@ function searchFiles(content) {
         let file2read = files[i]
         let fileContent = getFile(config.dataURL, file2read)
         for (var i = 0; i < content.length; i++) {
-            if (file2read.indexOf(content[i]) != -1) keywordCount += 1
-            if (fileContent.indexOf(content[i]) != -1) keywordCount += 1
+            if (file2read.match(content[i])) keywordCount += 1
+            if (fileContent.match(content[i])) keywordCount += 1
 
-            if (keywordCount == content.length) {
+            if (keywordCount >= content.length) {
                 result.push(getFileSummary(config.dataURL, file2read))
             }
         }
